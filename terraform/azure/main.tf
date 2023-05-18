6936db66181860c553fed899e1f90f63fb4a3761
rjonczy <robert.jonczy@gmail.com>
2023-05-18 23:04:02 +0200

    fixed apply

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "tendermint-rg" {
   name     = "tendermint-demo"
   location = "westeurope"
 }
+
+
+module "cluster" {
+  source           = "./cluster"
+  name             = "${var.NETWORK_NAME}"
+}
+
