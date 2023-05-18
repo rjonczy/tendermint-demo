384c60b4a6f18927903ab8f56cb271f2cdc4df04
rjonczy <robert.jonczy@gmail.com>
2023-05-18 22:51:27 +0200

    remove pr comments

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,9 +1,4 @@
-resource "azurerm_resource_group" "examplerg1" {
-  name     = "tendermint-demo-1"
-  location = "eastus2"
-}
-
-resource "azurerm_resource_group" "examplergi2" {
-  name     = "tendermint-demo-2"
-  location = "eastus2"
+resource "azurerm_resource_group" "tendermin-rg" {
+  name     = "tendermint-demo"
+  location = "westeurope"
 }
