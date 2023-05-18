52cb23512c5945670ce4c97ea0c8feb342e80d7f
rjonczy <robert.jonczy@gmail.com>
2023-05-18 22:20:58 +0200

    dummy rg to check gha workflow

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,5 +1,9 @@
-# Create Resource Group
-resource "azurerm_resource_group" "examplerg" {
-  name     = "tendermint-demo"
+resource "azurerm_resource_group" "examplerg1" {
+  name     = "tendermint-demo-1"
+  location = "eastus2"
+}
+
+resource "azurerm_resource_group" "examplergi2" {
+  name     = "tendermint-demo-2"
   location = "eastus2"
 }
